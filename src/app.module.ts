421a22e575aabc703d4bc8b9077669d0f39ccec6
Erhan1708 <erhankubanych@gmail.com>
2025-09-19 01:14:22 +0600

    Добавлены зависимости для админки и настроен доступ к базе данных через переменные окружения.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,9 @@ import { FileModule } from './file/file.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import * as path from 'path';
 import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 @Module({
   imports: [
     AlbumModule,
@@ -14,7 +16,7 @@ import { ConfigModule } from '@nestjs/config';
     FileModule,
     ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env'}),
-    MongooseModule.forRoot('mongodb+srv://erhan:gtx1708erhan@clustertunevaultserver.aweti.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTuneVaultServer')
+    MongooseModule.forRoot(process.env.MONGO_URL || '')
   ],
 })
 export class AppModule {}
